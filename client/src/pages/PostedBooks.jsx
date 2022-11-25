f8505f73bc45aac4d66e8d679e352430430bad99
Debraj Dhar <debrajdhar100@gmail.com>
2022-11-25 14:13:22 +0530

    bug fixes

--- client/src/pages/PostedBooks.jsx
+++ client/src/pages/PostedBooks.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../api/axios";
 import "../styles/PostedBooks.css";
 
 const PostedBooks = () => {
-  return <div className="posted-books-container container">PostedBooks</div>;
+  const getBooksUrL = "/api/get_books/";
+  const userId = window.localStorage.getItem("userId");
+  const userName = window.localStorage.getItem("userName");
+  const [postedBooks, setPostedBooks] = useState();
+  useEffect(() => {
+    getPostedBooks();
+  }, []);
+  const getPostedBooks = async () => {
+    axios
+      .get(getBooksUrL, { params: userId })
+      .then((res) => {
+        console.log(res.data);
+        setPostedBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  return <div className="posted-books-container container">Hello</div>;
 };
 
 export default PostedBooks;
